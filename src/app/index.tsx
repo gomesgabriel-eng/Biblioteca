825002a3ad8f199aeb1b1eab8c7d9e81ba984633
gomesgabriel-eng <gomesgabriel@acad.ifma.edu.br>
2026-07-08 19:51:44 -0300

    style: adiciona estilos básicos da tela

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Biblioteca IFMA</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Biblioteca IFMA</Text>
+      <Text style={styles.subtitle}>Seu conhecimento começa aqui</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#00843D",
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: "#666666",
+    marginTop: 5,
+  },
+});
